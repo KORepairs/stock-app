6e98b12b404292778afae08f34ea216360d07a68
KORepairs <korepairs18@gmail.com>
2026-02-08 19:28:45 +0000

    dashboard layout

--- src/index.js
+++ src/index.js
@@ -122,6 +122,8 @@ app.get('/tradeins',       (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'tra
 app.get('/report-sales',   (req, res) => {res.sendFile(path.join(__dirname, '..', 'public', 'report-sales.html'));});
 app.get('/inventory-add', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'inventory-add.html')));
 app.get('/report/ebay-updates', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'report-ebay-updates.html')));
+app.get('/dashboard', (req, res) => res.sendFile(path.join(PUBLIC_DIR, 'dashboard.html')));
+
 
 
 
